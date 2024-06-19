23edd304f7a228be5238d201bd7d27ba4ca36bea
“[Breno <manancialgd@gmail.com>
2024-06-19 20:55:24 +0100

    FirstCommit

---
+++ TriangTriang/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriangTriang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
